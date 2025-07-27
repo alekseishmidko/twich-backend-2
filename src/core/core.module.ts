d18828e62d6636c60a0abf0d6580b794e35bcc6b
alekseishmidko <alexeishmidko@gmail.com>
2025-07-28 00:34:54 +0700

    session init

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -5,16 +5,26 @@ import { IS_DEV_ENV } from '@/src/shared/utils/is-dev.util';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { getGraphQLConfig } from '@/src/core/config/graphql.config';
-
+import { RedisModule } from './redis/redis.module';
+import { AccountModule } from '@/src/modules/auth/account/account.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ignoreEnvFile: !IS_DEV_ENV, isGlobal: true}),
-    GraphQLModule.forRootAsync({
-    driver: ApolloDriver,
-    imports: [ConfigModule],
-    useFactory: getGraphQLConfig,
-    inject: [ConfigService],
-  }),PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV, // игнорировать .env файл, если не dev-окружение
+      isGlobal: true, // сделать ConfigModule доступным глобально
+    }),
 
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver, // указываем драйвер Apollo
+      imports: [ConfigModule], // импортируем ConfigModule (нужен для получения переменных)
+      useFactory: getGraphQLConfig, // используем фабрику для генерации конфигурации
+      inject: [ConfigService], // передаём зависимости в useFactory
+    }),
+
+    PrismaModule,
+    RedisModule,
+    AccountModule,
+  ],
 })
 export class CoreModule {}
